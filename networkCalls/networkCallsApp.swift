87f6f0d42ab4a6359d7d66f83944d18a7edb4cd6
Fateh Khan <fatehkhann@gmail.com>
2024-04-04 10:04:01 +0500

    Initial Commit

---
+++ networkCalls/networkCallsApp.swift
@@ -0,0 +1,17 @@
+//
+//  networkCallsApp.swift
+//  networkCalls
+//
+//  Created by Fateh Khan on 04/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct networkCallsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
